b514a006990c64b3fd26ac04956bee755565d2d4
최수혁 <choisuhyeok@choesuhyeog-ui-noteubug.local>
2024-08-21 22:24:19 +0900

    다시만듬

---
+++ src/header.svelte
@@ -0,0 +1,61 @@
+<script>
+</script>
+
+<header>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">About</a></li>
+            <li><a href="/">skills</a></li>
+            <li><a href="/">contact</a></li>
+        </ul>
+    </nav>
+</header>
+<main>
+    <div>
+        <img class="main_img" src="/src/img/IMG_0230.jpg" alt="사진">
+    </div>
+</main>
+
+
+<style>
+    :global(*){
+        padding: 0;
+        margin: 0;
+        list-style: none;
+        text-decoration: none;
+        
+    }
+    header{
+        position: fixed;
+        top: 0;
+        right: 0;
+    }
+    nav ul{
+        display: flex;
+        float: right;
+        justify-content: space-around;
+        width: 800px;
+        padding: 26px 0px;
+        background-color: black;
+        opacity: 0.6;
+        border-radius: 0 0 0 20px;
+        font-size: 16px;
+        box-shadow: 1px black;
+        
+    }
+    nav a {
+        color: grey;
+    }
+    nav ul:hover {
+        box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+        cursor: pointer;
+        transition: 0.3s;
+    }
+    nav a:hover{
+        color: white;
+        transition: 0.1s;
+    }
+    
+    
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 //import './app.css'
-import App from './nav.svelte'
+import App from './header.svelte'
 
 const app = new App({
   target: document.getElementById('app'),
